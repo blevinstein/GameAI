66a89e3132d38b8ac5628beee58303a25edc88ba
Brian Levinstein <blevinstein@gmail.com>
2014-10-18 14:13:33 -0700

    Add Point to xade

---
+++ com/blevinstein/xade/Point.java
@@ -0,0 +1,67 @@
+package com.blevinstein.xade;
+
+import java.util.Objects;
+
+public class Point {
+  private double x;
+  private double y;
+
+  public Point() {
+    this(0.0, 0.0);
+  }
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() { return x; }
+  public double getY() { return y; }
+
+  public Point add(Point other) {
+    return new Point(x + other.x, y + other.y);
+  }
+
+  public Point sub(Point other) {
+    return new Point(x - other.x, y - other.y);
+  }
+
+  public Point times(double scalar) {
+    return new Point(x * scalar, y * scalar);
+  }
+
+  public double mag2() {
+    return x*x + y*y;
+  }
+
+  public double mag() {
+    return Math.sqrt(mag2());
+  }
+
+  public static double dist(Point a, Point b) {
+    return a.sub(b).mag();
+  }
+
+  // interpolate(a, b, 0) => a
+  // interpolate(a, b, 1) => b
+  // interpolate(a, b, 0.5) => (a + b) / 2
+  public static Point interpolate(Point a, Point b, double t) {
+    // return a + (b - a) * t
+    return a.add(b.sub(a).times(t));
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Point) {
+      Point other = (Point) obj;
+      // TODO: add tolerance, should use equals on floating point #s
+      return x == other.x && y == other.y;
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(x, y);
+  }
+}
---
+++ com/blevinstein/xade/PointTest.java
@@ -0,0 +1,22 @@
+package com.blevinstein.xade;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PointTest {
+  @Test
+  public void testInterpolate() {
+    Point a = new Point(1.0, 2.0);
+    Point b = new Point(2.0, 3.0);
+    assertEquals(a, Point.interpolate(a, b, 0.0));
+    assertEquals(b, Point.interpolate(a, b, 1.0));
+    assertEquals(a.add(b).times(0.5), Point.interpolate(a, b, 0.5));
+  }
+
+  @Test
+  public void testMagnitude() {
+    Point a = new Point(3.0, 4.0);
+    assertEquals(a.mag(), 5.0, 0.001);
+  }
+}
