d0a54b9a1d5251f64d866aff2951c63d0b489b56
Brian Levinstein <blevinstein@gmail.com>
2014-10-18 23:29:01 -0700

    Improved Drawable interface.

--- com/blevinstein/xade/Drawable.java
+++ com/blevinstein/xade/Drawable.java
@@ -3,6 +3,7 @@ package com.blevinstein.xade;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 
 public interface Drawable {
   Shape shape();
@@ -14,11 +15,15 @@ public interface Drawable {
   }
 
   static void draw(Graphics2D g, Drawable d) {
+    draw(g, d, null);
+  }
+  static void draw(Graphics2D g, Drawable d, AffineTransform xfm) {
+    Shape shape = xfm == null ? d.shape() : xfm.createTransformedShape(d.shape());
     g.setColor(d.color());
     if (d.fill()) {
-      g.fill(d.shape());
+      g.fill(shape);
     } else {
-      g.draw(d.shape());
+      g.draw(shape);
     }
   }
 }
