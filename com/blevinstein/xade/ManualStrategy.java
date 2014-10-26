6ddddabccbb9800f3a3c9b0780edd6bec80a26ed
Brian Levinstein <blevinstein@gmail.com>
2014-10-26 12:57:54 -0700

    Add ManualStrategy for handling user input

---
+++ com/blevinstein/xade/ManualStrategy.java
@@ -0,0 +1,26 @@
+package com.blevinstein.xade;
+
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManualStrategy implements Strategy {
+  private World world;
+  private List<Move> moveBuffer = new ArrayList<>();
+
+  public ManualStrategy(World world) {
+    this.world = world;
+  }
+
+  public void makeMove(Move move) {
+    moveBuffer.add(move);
+  }
+
+  // implements Strategy
+  @Override
+  public List<Move> getMoves(World world) {
+    List<Move> moves = ImmutableList.copyOf(moveBuffer);
+    moveBuffer.clear();
+    return moves;
+  }
+}
