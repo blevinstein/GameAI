b1f53a15e3de255fc4c7128b186a1d2d94f3eb08
Brian Levinstein <blevinstein@gmail.com>
2014-10-26 02:00:43 -0700

    Add color to Player

--- com/blevinstein/xade/Player.java
+++ com/blevinstein/xade/Player.java
@@ -1,5 +1,6 @@
 package com.blevinstein.xade;
 
+import java.awt.Color;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -7,10 +8,14 @@ import java.util.function.Function;
 public class Player {
   private List<Army> armies;
   private Strategy strategy;
+  private Color color;
 
-  public Player(Strategy strategy) {
+  public Player(Color color, Strategy strategy) {
+    this.color = color;
     this.strategy = strategy;
   }
+
+  public Color getColor() { return color; }
 
   public List<Move> getMoves(World world) {
     return strategy.getMoves(world);
