1cd70cce42d15910797cba5535d595041b0c6b72
Brian Levinstein <blevinstein@gmail.com>
2014-10-26 17:51:17 -0700

    Cleanup Route

--- com/blevinstein/xade/Route.java
+++ com/blevinstein/xade/Route.java
@@ -3,7 +3,7 @@ package com.blevinstein.xade;
 import java.awt.Shape;
 
 // represents a route
-public abstract class Route implements Drawable {
+public abstract class Route {
   // returns the position of a point along the route at time t
   // NOTE: |dposition/dt| = 1, i.e. constant velocity of 1
   // NOTE: 0 < t < length()
@@ -12,7 +12,7 @@ public abstract class Route implements Drawable {
   public abstract Point direction(double t);
   // returns the total length of the route
   public abstract double length();
-  // implements Drawable
+  // returns a drawable representation of the route
   public abstract Shape shape();
 }
 
