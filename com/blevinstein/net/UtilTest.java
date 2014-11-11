02071bee1446b1ad149492c6358d443926c55b89
Brian Levinstein <blevinstein@gmail.com>
2014-11-10 21:28:20 -0800

    Add test for chain

---
+++ com/blevinstein/net/UtilTest.java
@@ -0,0 +1,28 @@
+package com.blevinstein.net;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+
+public class UtilTest {
+  @Test
+  public void testChain() {
+    assertEquals(
+        Util.chain(ImmutableList.of(1, 2, 3)),
+        ImmutableList.of(Pair.of(1, 2), Pair.of(2, 3)));
+  }
+
+  @Test
+  public void testChain_emptyList() {
+    // Empty list
+    assertEquals(
+        Util.chain(ImmutableList.<Integer>of()),
+        ImmutableList.<Pair<Integer,Integer>>of());
+    // Single element
+    assertEquals(
+        Util.chain(ImmutableList.of(5)),
+        ImmutableList.<Pair<Integer,Integer>>of());
+  }
+}
